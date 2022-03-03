ff2d3c25084802e0f5a8b5add4ada6ac9ded4d5e
Leonardo Augusto dos Santos Oliveira <Leonardoaugusto.soliveira@gmail.com>
2022-03-02 21:39:03 -0300

    adição das APIviews

---
+++ cursos/serializers.py
@@ -0,0 +1,18 @@
+from rest_framework import serializers
+from .models import Curso, Avaliacao
+
+
+class AvaliacaoSerializer(serializers.ModelSerializer):
+    class Meta:
+        extra_kwargs = {
+            'email': {'write_only': True}
+        }
+        model = Avaliacao
+        fields = ('email', 'curso', 'nome', 'email',
+                  'comentario', 'avaliacao', 'criacao', 'ativo')
+
+
+class CursoSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Curso
+        fields = ('id', 'titulo', 'url', 'criacao', 'ativo')
