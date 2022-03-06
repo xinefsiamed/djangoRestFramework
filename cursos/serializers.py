f316b50163ab11cf9689d48e3fc84a4c4d1d1c87
Leonardo Augusto dos Santos Oliveira <Leonardoaugusto.soliveira@gmail.com>
2022-03-06 12:54:59 -0300

    Adicionando relacionamento na API

--- cursos/serializers.py
+++ cursos/serializers.py
@@ -13,6 +13,16 @@ class AvaliacaoSerializer(serializers.ModelSerializer):
 
 
 class CursoSerializer(serializers.ModelSerializer):
+    # Nested Relationship
+    #avaliacoes = AvaliacaoSerializer(many=True, read_only=True)
+
+    # HyperLinked Related field
+    """ avaliacoes = serializers.HyperlinkedRelatedField(
+        many=True, read_only=True, view_name='avaliacao-detail') """
+
+    # PrimaryKey related field
+    avaliacoes = serializers.PrimaryKeyRelatedField(many=True, read_only=True)
+
     class Meta:
         model = Curso
-        fields = ('id', 'titulo', 'url', 'criacao', 'ativo')
+        fields = ('id', 'titulo', 'url', 'criacao', 'ativo', 'avaliacoes')
