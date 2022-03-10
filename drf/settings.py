448dde94762f962935a5438cdea14efa39ff8e4d
Leonardo Augusto dos Santos Oliveira <Leonardoaugusto.soliveira@gmail.com>
2022-03-10 19:52:59 -0300

    Adicionando Throttling na API

--- drf/settings.py
+++ drf/settings.py
@@ -142,5 +142,13 @@ REST_FRAMEWORK = {
     ),
     'DEFAULT_PERMISSION_CLASSES': (
         'rest_framework.permissions.IsAuthenticatedOrReadOnly',
-    )
+    ),
+    'DEFAULT_THROTTLE_CLASSES': (
+        'rest_framework.throttling.AnonRateThrottle',
+        'rest_framework.throttling.UserRateThrottle',
+    ),
+    'DEFAULT_THROTTLE_RATES': {
+        'anon': '5/minutes',  # second, day, month, year
+        'user': '10/minutes'
+    },
 }
