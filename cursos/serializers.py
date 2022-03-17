dc1719ed31dc242c1004586e9f45dc9f25d55a18
Leonardo Augusto dos Santos Oliveira <Leonardoaugusto.soliveira@gmail.com>
2022-03-17 17:59:36 -0300

    adiconando media às avaliações

--- cursos/serializers.py
+++ cursos/serializers.py
@@ -1,4 +1,6 @@
 from rest_framework import serializers
+from django.db.models import Avg
+
 from .models import Curso, Avaliacao
 
 
@@ -30,6 +32,18 @@ class CursoSerializer(serializers.ModelSerializer):
     # PrimaryKey related field
     avaliacoes = serializers.PrimaryKeyRelatedField(many=True, read_only=True)
 
+    media_avaliacoes = serializers.SerializerMethodField()
+
     class Meta:
         model = Curso
-        fields = ('id', 'titulo', 'url', 'criacao', 'ativo', 'avaliacoes')
+        fields = ('id', 'titulo', 'url', 'criacao',
+                  'ativo', 'avaliacoes', 'media_avaliacoes')
+
+    def get_media_avaliacoes(self, obj):
+        media = obj.avaliacoes.aggregate(
+            Avg('avaliacao')).get('avaliacao__avg')
+
+        if media is None:
+            return 0
+
+        return round(media * 2) / 2
